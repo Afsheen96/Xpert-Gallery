72d83b3192daa4abe0222b5dc2625d3f1ad93c8d
Afsheen Kiran <18mit29260@gmail.com>
2023-05-09 19:58:43 +0500

    add eslinters

--- xpert.js
+++ xpert.js
@@ -1,18 +1,17 @@
 const header = document.querySelector('.navbar');
-console.log(header)
-window.onscroll = function() {
-    const top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
-}
+console.log(header);
+window.onscroll = function () {
+  const top = window.scrollY;
+  if (top >= 100) {
+    header.classList.add('navbarDark');
+  } else {
+    header.classList.remove('navbarDark');
+  }
+};
 // collapse navbar after click on small devices
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
+const navLinks = document.querySelectorAll('.nav-item');
+const menuToggle = document.getElementById('navbarSupportedContent');
 
 navLinks.forEach((l) => {
-    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
-})
+  l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle(); });
+});
